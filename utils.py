ddedd6ccff4e00b64874cfe22cff563ff61e5488
Alex <alex.c.goble@gmail.com>
2023-01-11 23:09:14 -0500

    Endpoint for system stats

--- utils.py
+++ utils.py
@@ -7,6 +7,7 @@ from pathlib import Path
 from typing import Union
 
 import colorama
+import cpuinfo
 import psutil
 from colorama import Back, Fore, Style
 from pydantic import BaseModel
@@ -97,6 +98,73 @@ class Tools:
         options = options[:threads]
         mask = sum(options) if options else 1
         return mask
+
+    @staticmethod
+    def get_stats() -> dict:
+        def get_size(num: float) -> str:
+            for unit in ["B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB"]:
+                if abs(num) < 1024.0:
+                    return "{0:.1f}{1}".format(num, unit)
+                num /= 1024.0
+            return "{0:.1f}{1}".format(num, "YB")
+
+        def get_bar(perc: float, width: int = 20) -> str:
+            ratio = perc / 100
+            bar = "█" * round(ratio * width) + "-" * round(width - (ratio * width))
+            return f"|{bar}| {round(100 * ratio, 1)}%"
+
+        # -/-/-/CPU-/-/-/
+        cpu_count = psutil.cpu_count()  # Int
+        cpu_perc = psutil.cpu_percent(interval=3, percpu=True)  # List of floats
+        cpu_freq = psutil.cpu_freq(percpu=True)  # List of Objects
+        cpu_info = cpuinfo.get_cpu_info()  # Dict
+        cpu_type = cpu_info["brand_raw"] if "brand_raw" in cpu_info else "Unknown"
+
+        # -/-/-/MEM-/-/-/
+        ram = psutil.virtual_memory()  # Obj
+        ram_total = get_size(ram.total)
+        ram_used = get_size(ram.used)
+        disk = psutil.disk_usage(os.getcwd())
+        disk_total = get_size(disk.total)
+        disk_used = get_size(disk.used)
+
+        p = psutil.Process()
+        io_counters = p.io_counters()
+        disk_usage_process = io_counters[2] + io_counters[3]  # read_bytes + write_bytes
+        # Disk load
+        disk_io_counter = psutil.disk_io_counters()
+        if disk_io_counter:
+            disk_io_total = (
+                disk_io_counter[2] + disk_io_counter[3]
+            )  # read_bytes + write_bytes
+            disk_usage = (disk_usage_process / disk_io_total) * 100
+        else:
+            disk_usage = 0
+
+        # -/-/-/NET-/-/-/
+        net = psutil.net_io_counters()  # Obj
+        sent = get_size(net.bytes_sent)
+        recv = get_size(net.bytes_recv)
+
+        res = {
+            "cpu": {
+                "cores": cpu_count,
+                "percents": cpu_perc if isinstance(cpu_perc, list) else None,
+                "freq": [(i.current, i.max) for i in cpu_freq],
+                "bars": [get_bar(i) for i in cpu_perc] if cpu_perc else None,
+                "type": cpu_type,
+            },
+            "mem": {"used": ram_used, "total": ram_total, "bar": get_bar(ram.percent)},
+            "disk": {
+                "used": disk_used,
+                "total": disk_total,
+                "bar": get_bar(disk.percent),
+                "load": disk_usage,
+                "loadbar": get_bar(disk_usage),
+            },
+            "net": {"sent": sent, "recieved": recv},
+        }
+        return res
 
 
 class Const:
