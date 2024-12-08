cd6a52ec9aa87eeb5b61d8a20679e558d3465dd0
Vert <alex.c.goble@gmail.com>
2024-12-08 13:17:40 -0500

    update status bar to show map file being synced

--- common/statusbar.py
+++ common/statusbar.py
@@ -1,6 +1,7 @@
 import asyncio
 import os
 from itertools import cycle
+from pathlib import Path
 
 from .constants import BAR
 from .models import cache  # noqa
@@ -11,8 +12,9 @@ async def status_bar():
     await asyncio.sleep(5)
     global cache
     bar_cycle = cycle(BAR)
+    path = Path(str(cache.map_file))
     while True:
-        cmd = f"title ArkViewer {VERSION} {next(bar_cycle)}"
+        cmd = f"title ArkViewer {VERSION} - {path.stem} {next(bar_cycle)}"
         if cache.syncing:
             cmd += " [Syncing...]"
         os.system(cmd)
